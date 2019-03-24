1199438c07b17ee957190ca6c328aad2c95df20d
Miklos Niedermayer <miklosniedermayer@icloud.com>
2019-03-24 15:25:14 +0700

    Add circle ci

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # redis-scan-cli
+
+Filters redis keys on a glob pattern and performs actions on them. Right now, it is only possible to delete the found keys, or print them in verbose mode.
+
+* Doesn't use the redis KEYS command: safe to use on production systems
+* Handles non-ascii keys
+
+# Install
+
+1. Download a binary for your platform from the [releases page](https://github.com/miklosn/redis-scan-cli/releases).
+
+2. Make it executable and place it somewhere on PATH.
+
+# Usage
+
+```
+Usage: redis-scan-cli [options] <pattern>
+
+Options:
+  -V, --version            output the version number
+  --host <host>            Redis host (default: 127.0.0.1)
+  -p, --port <port>        Redis port (default: 6379)
+  -a, --auth <password>    Redis password (default: none)
+  -b, --batch <batchsize>  Batch size to use for scan (default 1000)
+  -d, --delete             Delete keys found
+  -v, --verbose            Set verbose logging
+  -h, --help               output usage information
+  ```
---
+++ redis-scan-cli.js
@@ -0,0 +1,125 @@
+const redis = require('redis');
+const program = require('commander');
+const jsesc = require('jsesc');
+const { Signale } = require('signale');
+const { promisify } = require('util');
+
+program
+  .version('0.1.0')
+  .arguments('<pattern>')
+  .option('--host <host>', 'Redis host (default: 127.0.0.1)')
+  .option('-p, --port <port>', 'Redis port (default: 6379)')
+  .option('-a, --auth <password>', 'Redis password (default: none)')
+  .option(
+    '-b, --batch <batchsize>',
+    'Batch size to use for scan (default 1000)',
+    parseInt
+  )
+  .option('-d, --delete', 'Delete keys found')
+  .option('-v, --verbose', 'Set verbose logging');
+
+program.parse(process.argv);
+
+if (!program.args || !program.args[0]) {
+  program.help();
+}
+
+const loggerOptions = {};
+
+const logger = new Signale(loggerOptions);
+
+const debug = message => {
+  if (program.verbose) {
+    logger.debug(message);
+  }
+};
+
+const options = {
+  host: '127.0.0.1',
+  port: 6379,
+  return_buffers: true
+};
+let batch = 1000;
+if (program.batch) {
+  batch = program.batch;
+}
+
+if (program.host) {
+  options.host = program.host;
+}
+
+if (program.port) {
+  options.port = program.port;
+}
+
+if (program.auth) {
+  options.password = program.auth;
+}
+
+const client = redis.createClient(options);
+const scanAsync = promisify(client.scan).bind(client);
+//const getAsync = promisify(client.get).bind(client);
+const deleteAsync = promisify(client.del).bind(client);
+
+client.on('error', function(err) {
+  logger.fatal(err);
+  process.exit(1);
+});
+
+const scan = async (cursor, pattern, reply) => {
+  const results = await scanAsync(cursor, 'MATCH', pattern, 'COUNT', batch);
+  let keys = results[1];
+  keys.forEach(key => {
+    reply.push(key);
+    debug(`Found "${jsesc(key.toString())}"`);
+  });
+  cursor = results[0].toString();
+  if (cursor !== '0') {
+    debug(`Cursor at ${cursor}`);
+    await scan(cursor, pattern, reply);
+  }
+};
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+const deleteKeys = async keys => {
+  await asyncForEach(keys, async key => {
+    await deleteAsync(key);
+    debug(`Deleted "${jsesc(key.toString())}"`);
+  });
+};
+
+exports.main = async () => {
+  try {
+    const client = await redis.createClient(options);
+    client.on('error', function(err) {
+      logger.fatal(err);
+      process.exit(1);
+    });
+    client.on('ready', function() {
+      // logger.debug(event);
+    });
+    let keys = [];
+    logger.time('scan');
+    await scan('0', program.args[0], keys);
+    logger.timeEnd('scan');
+    logger.complete(`Found ${keys.length} keys`);
+
+    if (program.delete) {
+      logger.time('delete');
+      await deleteKeys(keys);
+      logger.timeEnd('delete');
+      logger.complete(`Deleted ${keys.length} keys`);
+    }
+    process.exit(0);
+  } catch (error) {
+    logger.fatal(error);
+  }
+};
+
+if (module === require.main) {
+  exports.main();
+}
